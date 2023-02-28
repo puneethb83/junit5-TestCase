c4c13dec40e789a12b2932c26b2676de77d407ed
puneeth <puneethsmg05@gmail>
2023-02-28 19:53:40 +0530

    first commit

--- src/main/java/Demojunit/MathUtils.java
+++ src/main/java/Demojunit/MathUtils.java
@@ -1,9 +1,26 @@
 package Demojunit;
 
 public class MathUtils {
+	
+	
 	public int add(int a, int b) {
-		return a+b;
+		return a + b;
 	}
 	
+	public int subtraction(int a,int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+
+	public double ComputeCircleArea(double radius) {
+		return Math.PI * radius * radius;
+	}
 
 }
--- src/test/java/Demojunit/MathUtilsTest.java
+++ src/test/java/Demojunit/MathUtilsTest.java
@@ -1,18 +1,112 @@
 package Demojunit;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
+	MathUtils mathUtils;
+	
+//	@BeforeAll
+//	void beforeallinit() {
+//		System.out.println("This cases need to run before all");
+//	}
+//	
+//	@AfterAll
+//	 void afterallinit() {
+//		System.out.println("This cases need to clean after all");
+//	}
+	
+	
+    @BeforeEach
+	void init() { // ->create a method void in it
+      mathUtils = new MathUtils();
+
+	}
+    
+//    @AfterEach
+//    void cleanup() {
+//    	System.out.println("cleaning up...");
+//    }
+
+    @Nested
+    @DisplayName("add method")
+    class AddTest{
+    	
+  
 	@Test
-	void test() {
-		MathUtils mathUtils=new MathUtils();
-		int expected=3;
-		float actual=mathUtils.add(2, 1);
-		assertEquals(expected, actual);
+	@DisplayName("Adding two positive numbers")
+	void testAddPositive() {
+		//int expected = 3;
+		//int actual = mathUtils.add(2, 1);
+		assertEquals(3,mathUtils.add(2, 1), "Should return the right sum");
+
+	}
+	
+	@Test
+	@DisplayName("Adding two negative numbers")
+	void testAddNegative() {
+		//int expected = -3;
+		//int actual = mathUtils.add(-2, -1);
+		assertEquals(-3,mathUtils.add(-2,-1), "should return the right sum");
+	}
+	
+	@Test
+	@DisplayName("multiply methods")
+	void testMultiply() {
+		//assertEquals(4,mathUtils.multiply(2,2), "should return the right product");
+		
+		assertAll(
+				() -> assertEquals(4, mathUtils.multiply(2,2)),
+				() -> assertEquals(0, mathUtils.multiply(2,0)),
+				() -> assertEquals(-4, mathUtils.multiply(-2,2))
+				);
+			
+		
+	}
+	
+	
+
+	@Test
+	@EnabledOnOs(OS.LINUX)
+	void testdivide() {
+		
+		boolean isServerUp=false;
+		assumeTrue(isServerUp);
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide by zero throws Exception");
 
 	}
 
+	@Test
+	void ComputeCircleArea() {
+		assertEquals(314.1592653589793, mathUtils.ComputeCircleArea(10), "should return right circle area");
+	}
+	
+	@Test
+	@Disabled
+	@DisplayName("TDD method. Should not run")
+	void testdisabled() {
+		fail("this test should be disabled");
+	}
+
+}
 }
